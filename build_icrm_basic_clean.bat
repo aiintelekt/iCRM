a157bcbfb5ca23d2a0ed2cf4228bdb3acb8d8085
Afsar <afs.affu@gmail.com>
2024-09-09 16:43:11 +0530

    updating

--- build_icrm_basic_clean.bat
+++
@@ -1,5 +0,0 @@
-echo 'clean icrm package start....'
-
-for /d /r %%i in (*src*) do rmdir /s /q "%%i"
-
-echo 'clean icrm package end....'
--- build_icrm_basic_clean.sh
+++
@@ -1,5 +0,0 @@
-echo 'clean icrm package start....'
-
-find . -type d -name src -exec rm -r {} +
-
-echo 'clean icrm package end....'
